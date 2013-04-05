a5eee9835f1f6dc93501b8eb4fccef065ebcc9e9
Dennis Andersen <carl.dennis.andersen@gmail.com>
2013-04-05 02:08:00 +0200

    Main filen for all

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#ifdef GLARE_WINDOW_H
+#include <WindowShell.h>
+#else
+#include <Windows.h>
+#endif
+
+//#include <Python\ .h >
+//#include <D3DX11\ .h >
+
+int WINAPI WinMain( HINSTANCE hInst, HINSTANCE prevInst, PSTR cmdLine, int cmdShow)
+{
+	return cmdShow;
+}
